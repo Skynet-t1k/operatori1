e299ce7805087781ae81282ab9d3d90d60480e1e
Giuseppe Cattano <giuseppe.cattano@gmail.com>
2024-04-22 20:40:49 +0200

    completato esercizio operatori 1

---
+++ main.js
@@ -0,0 +1,8 @@
+let num1 = 3;
+let num2 = 6;
+let num3 = 9;
+let num4 = 12;
+let num5 = 16;
+let somma = num1 + num2 + num3 + num4 + num5;
+let media = somma/5;
+console.log(`La somma tra i numeri equivale a ${somma} e la media equivale a ${media}`);
